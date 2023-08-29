14b77755bd0398ba97f41f36f96114ad0e764cb5
Parker Boeing <parker.boeing@gmail.com>
2023-08-28 23:22:05 -0600

    feat: Get v0/markets passing postman

---
+++ app/serializers/market_serializer.rb
@@ -0,0 +1,9 @@
+class MarketSerializer
+  include JSONAPI::Serializer
+  attributes :name, :street, :city, :county, :state, :zip, :lat, :lon, :vendor_count
+  has_many :vendors
+
+  attribute :active do
+    true
+  end
+end
