dc0ba3d47cfc7a5930c1fcd6c953c98d311dba66
Parker Boeing <parker.boeing@gmail.com>
2023-08-29 11:14:31 -0600

    feat: Vendor show page

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -8,4 +8,13 @@ class Api::V0::VendorsController < ApplicationController
       render json: { errors: [{ detail: e.message }] }, status: :not_found
     end
   end
+
+  def show
+    begin
+      vendor = Vendor.find(params[:id])
+      render json: VendorSerializer.new(vendor)
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: [{ detail: e.message }] }, status: :not_found
+    end
+  end
 end
