283e90bb8cfdbca0f1213f8d58f342aac266d9a7
Parker Boeing <parker.boeing@gmail.com>
2023-08-28 23:58:23 -0600

    feat: Market show path, happy and sad

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -2,4 +2,13 @@ class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.new(Market.all)
   end
+
+  def show
+   market = Market.find_by(id: params[:id])
+   if market.nil?
+      render json: { errors: "The ID provided cannot be found" }, status: :not_found
+    else
+      render json: MarketSerializer.new(market)
+    end
+  end
 end
