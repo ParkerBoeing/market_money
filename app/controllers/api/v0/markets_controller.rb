8a4cea3f764829dd2fe12df59df6093291a09bcb
Parker Boeing <parker.boeing@gmail.com>
2023-08-30 20:39:49 -0600

    feat: Search for markets happy and sad

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -11,4 +11,47 @@ class Api::V0::MarketsController < ApplicationController
       render json: { errors: [{ detail: e.message }] }, status: :not_found
     end
   end
+
+  def search
+    markets = Market.all
+    if params[:state] && !params[:city] && !params[:name]
+      markets = markets.where("state ILIKE ?", "%#{params[:state]}%")
+    end
+
+    if params[:name] && !params[:city] && !params[:state]
+      markets = markets.where("name ILIKE ?", "%#{params[:name]}%")
+    end
+
+    if params[:state] && params[:city] && !params[:name]
+      markets = markets.where("state ILIKE ? AND city ILIKE ?", 
+                              "%#{params[:state]}%", 
+                              "%#{params[:city]}%"
+                              )
+    end
+
+    if params[:state] && params[:name] && !params[:city]
+      markets = markets.where("state ILIKE ? AND name ILIKE ?", 
+                              "%#{params[:state]}%", 
+                              "%#{params[:name]}%"
+                              )
+    end
+
+    if params[:state] && params[:city] && params[:name]
+      markets = markets.where("state ILIKE ? AND city ILIKE ? AND name ILIKE ?", 
+                              "%#{params[:state]}%", 
+                              "%#{params[:city]}%", 
+                              "%#{params[:name]}%"
+                              )
+    end
+
+    if params[:city] && !params[:state] && !params[:name]
+      render json: { errors: [{ detail: "Cannot send in just city params" }] }, status: 422 and return
+    end
+
+    if params[:city] && params[:name] && !params[:state]
+      render json: { errors: [{ detail: "Cannot send in just city and name params" }] }, status: 422 and return
+    end
+
+    render json: MarketSerializer.new(markets)
+  end 
 end
