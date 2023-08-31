b50770bc3fea95e0e79b592f8207e9cd6f2975e6
Parker Boeing <parker.boeing@gmail.com>
2023-08-30 23:47:47 -0600

    test: Market show happy and sad path

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -47,6 +47,54 @@ describe "Markets API" do
   end
 
   it "sends a market" do
-    
+    market = create(:market)
+
+    get "/api/v0/markets/#{market.id}"
+
+    expect(response).to be_successful
+    parsed_body = JSON.parse(response.body, symbolize_names: true)
+    market = parsed_body[:data]
+
+    expect(market).to have_key(:id)
+    expect(market[:id]).to be_a(String)
+
+    market = market[:attributes]
+
+    expect(market).to have_key(:name)
+    expect(market[:name]).to be_a(String)
+
+    expect(market).to have_key(:street)
+    expect(market[:street]).to be_a(String)
+
+    expect(market).to have_key(:city)
+    expect(market[:city]).to be_a(String)
+
+    expect(market).to have_key(:county)
+    expect(market[:county]).to be_a(String)
+
+    expect(market).to have_key(:state)
+    expect(market[:state]).to be_a(String)
+
+    expect(market).to have_key(:zip)
+    expect(market[:zip]).to be_a(String)
+
+    expect(market).to have_key(:lat)
+    expect(market[:lat]).to be_a(String)
+
+    expect(market).to have_key(:lon)
+    expect(market[:lon]).to be_a(String)
+
+    expect(market).to have_key(:vendor_count)
+    expect(market[:vendor_count]).to be_an(Integer)
+  end
+
+  it "has a market show sad path" do
+    get '/api/v0/markets/9999999'
+
+    expect(response).to have_http_status(404)
+
+    parsed_response = JSON.parse(response.body)
+
+    expect(parsed_response).to eq({"errors"=>[{"detail"=>"Couldn't find Market with 'id'=9999999"}]})
   end
 end
