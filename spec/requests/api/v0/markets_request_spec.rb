4a76691b5eeb8854ed2dd04f3b6396e037ed510b
Parker Boeing <parker.boeing@gmail.com>
2023-08-30 23:34:19 -0600

    test: Markets send

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -7,7 +7,46 @@ describe "Markets API" do
     get '/api/v0/markets'
 
     expect(response).to be_successful
-    parsed_body = JSON.parse(response.body)
-    expect(parsed_body["data"].size).to eq(10)
+    parsed_body = JSON.parse(response.body, symbolize_names: true)
+    markets = parsed_body[:data]
+    expect(markets.size).to eq(10)
+
+    markets.each do |market|
+      expect(market).to have_key(:id)
+      expect(market[:id]).to be_a(String)
+
+      market = market[:attributes]
+
+      expect(market).to have_key(:name)
+      expect(market[:name]).to be_a(String)
+
+      expect(market).to have_key(:street)
+      expect(market[:street]).to be_a(String)
+
+      expect(market).to have_key(:city)
+      expect(market[:city]).to be_a(String)
+
+      expect(market).to have_key(:county)
+      expect(market[:county]).to be_a(String)
+
+      expect(market).to have_key(:state)
+      expect(market[:state]).to be_a(String)
+
+      expect(market).to have_key(:zip)
+      expect(market[:zip]).to be_a(String)
+
+      expect(market).to have_key(:lat)
+      expect(market[:lat]).to be_a(String)
+
+      expect(market).to have_key(:lon)
+      expect(market[:lon]).to be_a(String)
+
+      expect(market).to have_key(:vendor_count)
+      expect(market[:vendor_count]).to be_an(Integer)
+    end
+  end
+
+  it "sends a market" do
+    
   end
 end
